d69a87c0f60d75f516a9dba086c3d2c87870deea
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-04-25 15:52:42 +0100

    add first hook to firebase DB and renders stringyfied data in the DOM

--- src/components/pages/Poll.js
+++ src/components/pages/Poll.js
@@ -4,8 +4,21 @@
 import React, { Component } from 'react';
 
 import { database } from '../../database/firebase';
-console.log(database)
+
 import { NewButton } from '../Button';
+
+/**
+ * "rules":{ ".read": "true" , ".write": "true"}
+ */
+try {
+    // debug - gets /hello node
+    database.ref('/').child('/hello').on('value', (snapshot) => {
+        console.log('ON - hello node: ', snapshot.val());
+    });
+}
+catch(err) {
+    console.log(err)
+}
 
 
 class Poll extends Component {
@@ -16,18 +29,29 @@ class Poll extends Component {
             data: null,
             newData: null
         }
+
     }
 
     componentDidMount() {
 
+        database.ref('/').once('value')
+            .then((snapshot) => {
+            const data = snapshot.val()
+                this.setState({data})
+                console.log('STATE:', this.state)
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     }
 
     render() {
 
         return(
-            <div>
+            <section className="poll-page">
                 <NewButton />
-            </div>
+                { JSON.stringify(this.state.data, null, 2) }
+            </section>
         )
     }
 
