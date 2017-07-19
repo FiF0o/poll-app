922d5a607b1602a87f8793cfce435a189c557248
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-07-19 23:18:34 +0100

    add boilerplate code for additional SW (not using RCA)

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,64 @@
 /**
  * Created by jonlazarini on 15/07/17.
  */
+// https required
+
+/**
+ * Boilerplate code to register service workers in our entrypoint
+ */
+let SW_FILE = './service-worker.js';
+
+// register sw
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register(SW_FILE, {scope: './'}) // where/scope our sw to be run
+        .then((registration) => console.log(`Service Worker registred: ${registration}`))
+        .catch((err) => console.error(`Service Worker failed to register: ${err}`))
+}
 console.log('service worker file loaded!');
+
+
+/**
+ * Boilerplate code for our SW
+ */
+const cacheName = 'v1'; // cache busting & versioning
+let cacheFiles = [
+    // files to be cached
+    './index.css',
+    'index.js'
+];
+
+self.addEventListener('install', (e) => {
+   console.log(`[SW] Installed`);
+    // to be resolved first
+    e.waitUntil(
+       caches.open(cacheName)
+           .then((cache) => {
+                console.log(`[SW] ...Caching files...`)
+               return cache.addAll(cacheFiles)
+           })
+    )
+   console.log(`[SW] Files successfully cached!`)
+});
+
+self.addEventListener('activate', (e) => {
+    console.log(`[SW] Activated`);
+    // make sure old cached files don't remain
+    e.waitUntil(
+        caches.keys()
+            .then((caNames) => {
+                console.log(`[SW] cacheNames: ${caNames}`);
+                return Promise.all(caNames.map((c) => {
+                    if (c !== cacheName) {
+                        console.log(`[SW] Removing cached files from ${c}`)
+                        return caches.delete(c)
+                    }
+                }))
+            })
+    )
+});
+
+self.addEventListener('fetch', (e) => {
+    console.log(`[SW] Fetching...`)
+    //TODO Ajax example here to cache image(s)
+});
