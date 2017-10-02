26f9b8c98edda145e48cf348f0cd259a39fcd138
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-10-02 15:30:11 +0100

    add unit tests for auth reducer

---
+++ src/reducers/auth.test.js
@@ -0,0 +1,56 @@
+import auth from './auth';
+
+describe('auth reducer', () => {
+    it('should handle initial state', () => {
+        expect(
+            auth(undefined, {})
+        )
+        .toEqual({
+            status: 'ANONYMOUS',
+            email: null,
+            displayName: null,
+            photoURL: null,
+            uid: null
+        })
+    })
+
+    it('should handle SIGN_IN', () => {
+        expect(
+            auth(undefined, {
+                type: 'SIGN_IN',
+                email: 'mail@mail.com',
+                displayName: 'First',
+                photoURL: 'image.jpg',
+                uid: '123' 
+            })
+        )
+        .toEqual({
+            status: 'SIGNED_IN',
+            email: 'mail@mail.com',
+            displayName: 'First',
+            photoURL: 'image.jpg',
+            uid: '123' 
+        })
+    })
+
+    it('should handle SIGN_OUT', () => {
+        expect(
+            auth({
+                status: 'SIGNED_IN',
+                email: 'mail@mail.com',
+                displayName: 'First',
+                photoURL: 'image.jpg',
+                uid: '123' 
+            }, {
+                type: 'SIGN_OUT'
+            })
+        )
+        .toEqual({
+            status: 'ANONYMOUS',
+            email: null,
+            displayName: null,
+            photoURL: null,
+            uid: null
+        })
+    })
+})
