4b2687669fd1efa121e9194d537335f3d9bdeddf
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-10-03 15:59:33 +0100

    mock firebase in browser for tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,6 @@
 // import 'jest-enzyme';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
-
+// Mock firebase in browser as tests throw errors otherwise, remove --browser flag in test task
+jest.mock('firebase', () => require('firebase/firebase-browser.js'));
 configure({ adapter: new Adapter() });
