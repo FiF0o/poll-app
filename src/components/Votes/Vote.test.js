0fe2fde1ba7df917c06a6dcc688d2b453611f8f3
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-10-03 18:25:45 +0100

    add tests for Vote component

---
+++ src/components/Votes/Vote.test.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import { Vote } from './Vote'
+
+function setup() {
+    let component;
+    const props = {
+        name: 'name'
+    }
+
+    component = mount(
+        <MuiThemeProvider>
+            <Vote {...props}/>
+        </MuiThemeProvider>
+    )
+
+    return {
+        props,
+        component
+    }
+}
+
+describe(' <Vote/> component', () => {
+    it('should render self and its subcomponents', () => {
+        const { component } = setup()
+        expect(component.length).toBe(1)
+    })
+
+    it('should render the name of the voter', () => {
+        const { component } = setup()
+        expect(component.find('span').text()).toBe('name')
+    })
+
+    it('should not render the name if the props.name is undefined', () => {
+        const props = {
+            name: undefined
+        }
+        const component = mount(
+            <MuiThemeProvider>
+                <Vote {...props} />
+            </MuiThemeProvider>
+        )
+
+        expect(component.find('span').text()).not.toBe(true)
+    })
+})
