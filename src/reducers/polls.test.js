98e845a08f7b66638b50a67a2bc485b22e84ac15
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-11-11 11:51:34 +0000

    add tests for polls reducers, firebase needs to be mocked

---
+++ src/reducers/polls.test.js
@@ -0,0 +1,69 @@
+import polls from './polls';
+
+// mock date - Date extended so the ts is frozen.
+const Date = require('./__mocks__/date').__createDate
+
+describe('polls reducer', () => {
+
+    it('should handle initial state', () => {
+        expect(
+            polls(undefined, {})
+        )
+        .toEqual({
+            byId: {},
+            allIds: []
+        })
+    })
+
+    it('should handle ADD_POLL', () => {
+        expect(
+            polls({}, {
+                type: 'ADD_POLL',
+                name: 'poll name',
+                description: 'Marv Luvvhini est un peu un vier.',
+                timeStamp: Date(),
+                author: 'username 1',
+                uid: 'user1',
+                id: 'poll1',
+                voters: []
+            })
+        )
+        .toEqual({
+            byId: {
+                poll1: {
+                    name: 'poll name',
+                    description: 'Marv Luvvhini est un peu un vier.',
+                    timeStamp: Date(),
+                    author: 'username 1',
+                    uid: 'user1',
+                    id: 'poll1',
+                    voters: []
+                }
+            },
+            allIds:['poll1']
+        })
+    })
+
+    it('should handle REMOVE_POLL', () => {
+        expect(
+            polls({
+                byId: {
+                    poll1: {
+                        name: 'poll name',
+                        description: 'Marv Luvvhini est un peu un vier.',
+                        timeStamp: Date(),
+                        author: 'username 1',
+                        uid: 'user1',
+                        id: 'poll1',
+                        voters: []
+                    }
+                },
+                allIds:['poll1']
+            }, {
+                type: 'REMOVE_POLL',
+                id: 'poll1'
+            })
+        )
+        .toEqual({byId:{}, allIds:[]})
+    })
+})
