ee53cedaf5d6379dfb125bad13b8cedc250305c1
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-11-12 19:22:36 +0000

    add test for timer

--- src/initializers/tick.test.js
+++ src/initializers/tick.test.js
@@ -1,8 +1,9 @@
 // import configureMockStore from 'redux-mock-store';
 import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk';
 import tick from './tick';
 
-jest.useFakeTimers();
+const clock = jest.useFakeTimers();
 
 describe('tick initializer', () => {
     it('should dispatch actions every 3 seconds', () => {
@@ -13,15 +14,15 @@ describe('tick initializer', () => {
        
         expect(setInterval.mock.calls.length).toBe(1);
         expect(setInterval.mock.calls[0][1]).toBe(3000);
+        // need to restore()
     });
 
-    // it('should dispatch a TICK action type', () => {
-    //     const store = configureStore([])({})
-    //     tick(store);
+    it('should dispatch a TICK action type', () => {
+        const store = configureStore([thunk])({})
+        const expectedPayload = { type: 'TICK' }
+        const cb = tick(store.dispatch(expectedPayload))
+        const actions = store.getActions()
        
-    //     const actions = store.getActions()
-    //     const expectedPayload = { type: 'TICK' }
-        
-    //     expect(actions).toEqual(expectedPayload)
-    // });
+        expect(actions).toEqual([expectedPayload])
+    });
 });
