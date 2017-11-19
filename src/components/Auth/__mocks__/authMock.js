71628480971d3f034ec52139b536a8447af8179e
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-11-19 14:33:38 +0100

    add enzyme, setupTests file and test for auth component

---
+++ src/components/Auth/__mocks__/authMock.js
@@ -0,0 +1,10 @@
+export const authMock = {
+  email: 'mail@mail.com',
+  displayName: 'displayName',
+  photoURL: 'image.jpg',
+  uid: '123',
+  signIn: jest.fn(),
+  signOut: jest.fn(),
+  isLoading: false,
+  errorMessage: null
+}
---
+++ src/components/Auth/auth.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {Auth} from './';
+import {authMock} from './__mocks__/authMock';
+
+describe('<Auth /> component', () => {
+  it('should render correctly', () => {
+    const stub = {
+      ...authMock
+    }
+    const wrapper = shallow(<Auth {...stub} />);
+    expect(wrapper.length).toEqual(1);
+  });
+});
